642c2eebc19a8b3289bceb51ceb4d03119ff9c7c
Osman Zakir <osmanzakir90@hotmail.com>
2021-03-02 02:59:11 +0500

    fixed linter errors

--- server.js
+++ server.js
@@ -6,21 +6,22 @@ const express = require("express");
 const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 const cors = require("cors");
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", (req, res) => {
-  res.sendFile(`${__dirname}/views/index.html`);
+  const path = `${__dirname}`;
+  res.sendFile(path.join("/views/index.html"));
 });
 
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", (req, res) => {
-  res.json({greeting: "hello API"});
+  res.json({ greeting: "hello API" });
 });
 
 // listen for requests :)
