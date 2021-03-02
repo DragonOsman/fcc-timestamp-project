09e1b92e8b9ed50979dd2456fa0eefeffe15a1f1
Osman Zakir <osmanzakir90@hotmail.com>
2021-03-02 23:17:05 +0500

    removed .env

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 
 // init project
 const express = require("express");
+const path = require("path");
 const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
@@ -15,8 +16,7 @@ app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", (req, res) => {
-  const path = `${__dirname}`;
-  res.sendFile(path.join("/views/index.html"));
+  res.sendFile(path.join(`${__dirname}`, "/views/index.html"));
 });
 
 // your first API endpoint...
@@ -27,4 +27,37 @@ app.get("/api/hello", (req, res) => {
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
   console.log(`Your app is listening on port ${listener.address().port}`);
+});
+
+// if date parameter is empty, return JSON response with both unix and utc keys
+app.get("/api/timestamp", (req, res) => {
+  const currentDateTime = new Date();
+  res.json({
+    unix: currentDateTime.valueOf(),
+    utc: currentDateTime.toUTCString()
+  });
+});
+
+/** A request to /api/timestamp/:date? with a
+ *   valid date should return a JSON object with
+ *   a unix key that is a Unix timestamp of the
+ *   input date in milliseconds
+ *   A request to /api/timestamp/:date? with
+ *   a valid date should return a JSON object
+ *   with a utc key that is a string of the
+ *   input date in the format: Thu, 01 Jan 1970 00:00:00 GMT
+ */
+app.get("/api/timestamp/:date", (req, res) => {
+  const dateQuery = req.params.date;
+  // check if string is a timestamp
+  if (/\d{5,}/.test(dateQuery)) {
+    res.json({ unix: parseInt(dateQuery), utc: new Date(parseInt(dateQuery)).toUTCString() });
+  } else {
+    if (new Date(dateQuery).toString() !== "Invalid Date") {
+      const dateStr = new Date(dateQuery);
+      res.json({ unix: dateStr.valueOf(), utc: dateStr.toUTCString() });
+    } else {
+      res.json({ error: "Invalid Date" });
+    }
+  }
 });
